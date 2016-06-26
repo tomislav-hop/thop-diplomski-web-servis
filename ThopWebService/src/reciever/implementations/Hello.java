49163a497ceaf8efdbe53f72cd101ee5675d6afe
tomislav-hop <tomislav.hop@gmail.com>
2016-06-26 11:29:28 +0200

    Test Commit

--- ThopWebService/src/reciever/implementations/Hello.java
+++ ThopWebService/src/reciever/implementations/Hello.java
@@ -8,7 +8,7 @@ public class Hello implements InterfaceHello {
 	public String sayHello(String name) {
 
 		System.out.println("Hello: " + name);
-		return "Hello: " + name;
+		return "Hello: " + name + " test";
 	}
 
 	@Override
